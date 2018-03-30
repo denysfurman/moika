2c5be69b55a1fe133b6f973379672fa6d137b314
denis <furik1102@gmail.com>
2018-03-30 13:07:25 +0300

    about

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,28 @@
 $(function() {
 
-
+    //main_slider
     $('.main-slider__wrap').owlCarousel({
         loop: true,
         nav:true,
         items:1
     });
+    //header scroll
+    $(window).scroll(function () {
+        var sc = $(window).scrollTop()
+        if (sc > 80) {
+            $(".main-header__topline").addClass("header-sroll")
+        } else {
+            $(".main-header__topline").removeClass("header-sroll")
+        }
+    });
 
 
-
+    //mob menu
     $('.menu-button').on('click', function() {
 
         $(this).toggleClass('active');
         $('.main-header__topline_nav').stop(true).slideToggle();
+        return false;
 
 
     });
