20f3867899d16a11ea90eaa0998e89c14f844c02
denis <furik1102@gmail.com>
2018-04-04 17:30:24 +0300

    all_5/04

---
+++ links.sh
@@ -0,0 +1,3 @@
+ln -s /Users/denis/PhpstormProjects/moika/project/storage/app /Users/denis/PhpstormProjects/moika/project/public/uploads
+
+ln -s /Users/denis/PhpstormProjects/moika/project/vendor/crocodicstudio/crudbooster/src/commands/../assets /Users/denis/PhpstormProjects/moika/project/public/vendor/crudbooster
